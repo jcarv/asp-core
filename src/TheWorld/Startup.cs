36104ecd6cef8d8bdd276e68ad6b6d97537d5235
João Carvalho <jmiguel.carv@gmail.com>
2016-12-08 20:15:34 +0000

    Lesson 2: HTML and CSS Basics

--- src/TheWorld/Startup.cs
+++ src/TheWorld/Startup.cs
@@ -28,10 +28,8 @@ namespace TheWorld
                 app.UseDeveloperExceptionPage();
             }
 
-            app.Run(async (context) =>
-            {
-                await context.Response.WriteAsync("Hello World!");
-            });
+            app.UseDefaultFiles();
+            app.UseStaticFiles();
         }
     }
 }
